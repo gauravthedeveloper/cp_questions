1653d8c2d1cfff6ebcca1b746128f8301cc2ac13
gauravthedeveloper <makecocgreatagain@gmail.com>
2023-04-27 23:11:12 +0530

    changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #pragma region region1
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 using namespace std;
 
 #define ff first
@@ -12,7 +10,10 @@ using namespace std;
 #define pb push_back
 #define mp make_pair
 #define pii pair<int, int>
+#define pic pair<int, char>
+#define pci pair<char, int>
 #define vi vector<int>
+#define vc vector<char>
 #define li list<int>
 #define vvi vector<vector<int>>
 #define mii map<int, int>
@@ -20,6 +21,8 @@ using namespace std;
 #define pqs priority_queue<int, vi, greater<int>>
 #define fl(n) for (int i = 0; i < n; i++)
 #define flj(n) for (int j = 0; j < n; j++)
+#define upper(s1) transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define lower(s1) transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
 #define setbits(x) __builtin_popcountll(x)
 #define zrobits(x) __builtin_ctzll(x)
 #define mod 1000000000
@@ -32,8 +35,8 @@ using namespace std;
     int x;    \
     cin >> x; \
     while (x--)
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 string getString(char x)
 {
     string s(1, x);
@@ -67,11 +70,12 @@ vi fib(int kk)
 #pragma endregion region1;
 void solve()
 {
+    cout << "hello" << endl;
 }
 #pragma region region2
 signed main()
 {
-    // sectumsempra07();
+    sectumsempra07();
     int t = 1;
     // cin >> t;
     while (t--)
